c25ac2ba9ff4c29239f1f2725f0a6e9e998b47e5
mukitur <mukit.inway@gmail.com>
2022-07-25 19:16:32 +0600

    creates hooks & components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,36 @@
-import React from "react";
+import { Grid, Stack } from "@mui/material";
+import CssBaseline from "@mui/material/CssBaseline";
+import { Container } from "@mui/system";
+import Navbar from "./components/navbar";
+import PlaylistCardItem from "./components/playlist-card-item";
+import usePlaylists from "./hooks/usePlaylists";
 
 const App = () => {
+  const { playlists, error, getPlaylistById } = usePlaylists();
+
+  const playlistArray = Object.values(playlists);
+
   return (
-    <div>
-      <h2>welcome</h2>
-    </div>
+    <>
+      <CssBaseline />
+      <Container maxWidth={"lg"} sx={{ my: 16 }}>
+        <Navbar getPlaylistById={getPlaylistById} />
+        {playlistArray.length > 0 && (
+          <Grid container alignItems="stretch">
+            {playlistArray.map((item) => (
+              <Grid item xs={12} md={6} lg={4} mb={2}>
+                <PlaylistCardItem
+                  key={item.id}
+                  playlistThumbnail={item.playlistThumbnail}
+                  playlistTitle={item.playlistTitle}
+                  channelTitle={item.channelTitle}
+                />
+              </Grid>
+            ))}
+          </Grid>
+        )}
+      </Container>
+    </>
   );
 };
 
